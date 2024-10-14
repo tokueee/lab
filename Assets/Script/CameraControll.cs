615c6d7a43ba551a0275d0dec49e44abd1f6ce81
tokueee <shibashibamasaki@i.softbank.jp>
2024-10-14 11:34:40 +0900

    カメラが動かせるようにSprictを追加

---
+++ Assets/Script/CameraControll.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll: MonoBehaviour
+{
+    //追従して欲しいオブジェクト
+    public Transform target;
+    public Vector3 offset;
+
+    private float RotateSpeed = 1.5f;
+
+    private Vector3 Rot_Camera = Vector2.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Cursor.visible = false;
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //カメラリセット
+        if (Input.GetKey(KeyCode.R))
+        {
+            Rot_Camera = Vector2.zero;
+        }
+
+        transform.position = target.position + offset;
+
+        Rot_Camera.x -= Input.GetAxis("Mouse Y") * RotateSpeed;
+        Rot_Camera.x = Mathf.Clamp(Rot_Camera.x, -90, 90);
+        Rot_Camera.y += Input.GetAxis("Mouse X") * RotateSpeed;
+
+        transform.rotation = Quaternion.Euler(Rot_Camera.x, Rot_Camera.y, 0);
+    }
+}
