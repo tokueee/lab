e97f032a6c1da18577e7d2d7d7eeabab72c8c581
Suzuki-Itsuki <s2223134@cco.kanagawa-it.ac.jp>
2024-10-21 11:26:02 +0900

    キャラクターの速度をほぼ一定になるようにしました。 また、左Shiftで走れるようにしました。 SaveSpeed変数をいじれば最高速度を変えられます。

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody rb;
-    float mspeed = 10f;
+    float mspeed = 3f;//加速度変数
+    int SaveSpeed = 5;//速度制御用変数
     public bool flag = false;
     
     // Start is called before the first frame update
@@ -37,6 +38,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        //速度を検出して制御するif文
+        if (rb.velocity.magnitude > SaveSpeed)
+        {
+            mspeed = -8;
+        }
+        else if (rb.velocity.magnitude < SaveSpeed && rb.velocity.magnitude > 0f)
+        {
+            mspeed += SaveSpeed;
+        }
+        else if (rb.velocity.magnitude <= 0f)
+        {
+            mspeed = SaveSpeed;
+        }
+
         //移動
         //前に進む
         if (Input.GetKey(KeyCode.W))
@@ -80,6 +96,16 @@ public class Player : MonoBehaviour
             {
                 rb.velocity = Vector3.zero;
             }
+        }
+
+        //Shiftキーでダッシュする
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            SaveSpeed = 5;
+        }
+        else
+        {
+            SaveSpeed = 3;
         }
     }
 }
