3c14e2281941173545f0ce8f68edba1894d1674d
Suzuki-Itsuki <s2223134@cco.kanagawa-it.ac.jp>
2024-10-29 14:43:12 +0900

    Playerの動きの使用に大きな変更を加えました。 キャラクターのバクを一部修正しました。

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,8 +6,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody rb;
+    CameraControll CameraCS;
+    Vector3 get_see;
+
+    int keyCount;
+    bool key_W, key_A, key_S, key_D;
+    float[] global_x = new float[5], global_y = new float[5], global_z = new float[5];//移動制御変数
     float mspeed = 3f;//加速度変数
-    int SaveSpeed = 5;//速度制御用変数
+    //※int SaveSpeed = 5;//速度制御用変数
     /*public bool flag = false;
     public bool flag2 = false;*/
     public bool[] flags;
@@ -28,6 +34,7 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         player_light = Light.GetComponent<Light>();
         UpdateLight();
+        CameraCS = GetComponent<CameraControll>();
 
     }
     private void OnCollisionEnter(Collision collision)
@@ -60,79 +67,157 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        get_see = CameraCS.Rot_Camera;
+        //各速度の値を毎度初期化
+        keyCount = 0;
+        key_W = false; key_A = false; key_S = false; key_D = false;
+        for (int i = 0; i < 5; i++)
+        {
+            global_x[i] = 0; global_y[i] = 0; global_z[i] = 0;
+        }
 
-        //速度を検出して制御するif文
-        if (rb.velocity.magnitude > SaveSpeed)
-        {
-            mspeed = -8;
-        }
-        else if (rb.velocity.magnitude < SaveSpeed && rb.velocity.magnitude > 0f)
-        {
-            mspeed += SaveSpeed;
-        }
-        else if (rb.velocity.magnitude <= 0f)
-        {
-            mspeed = SaveSpeed;
+        //※速度を検出して制御するif文
+        {/*
+            if (rb.velocity.magnitude > SaveSpeed)
+            {
+                mspeed = -8;
+            }
+            else if (rb.velocity.magnitude < SaveSpeed && rb.velocity.magnitude > 0f)
+            {
+                mspeed += SaveSpeed;
+            }
+            else if (rb.velocity.magnitude <= 0f)
+            {
+                mspeed = SaveSpeed;
+            }*/
         }
 
         //移動
-        //前に進む
-        if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(transform.forward * mspeed);
+            //前に進む
+            if (Input.GetKey(KeyCode.W))
+            {
+                keyCount += 1;
+                key_W = true;
+                //rb.AddForce(transform.forward * mspeed);
+                //global_z[1] = mspeed * Mathf.Cos(get_sea.y/180*Mathf.PI);
+                //global_x[1] = mspeed * Mathf.Sin(get_sea.y / 180 * Mathf.PI);
+            }
+            //後に進む
+            if (Input.GetKey(KeyCode.S))
+            {
+                keyCount += 1;
+                key_S = true;
+                //rb.AddForce(-transform.forward * mspeed);
+                //global_z[3] = -mspeed * Mathf.Cos(get_sea.y / 180 * Mathf.PI);
+                //global_x[3] = -mspeed * Mathf.Sin(get_sea.y / 180 * Mathf.PI);
+            }
+            //右に進む
+            if (Input.GetKey(KeyCode.D))
+            {
+                keyCount += 1;
+                key_D = true;
+                //rb.AddForce(transform.right * mspeed);
+                //global_z[4] = mspeed * Mathf.Cos((get_sea.y+90) / 180 * Mathf.PI);
+                //global_x[4] = mspeed * Mathf.Sin((get_sea.y+90) / 180 * Mathf.PI);
+            }
+            //左に進む
+            if (Input.GetKey(KeyCode.A))
+            {
+                keyCount += 1;
+                key_A = true;
+                //rb.AddForce(-transform.right * mspeed);
+                //global_z[2] = -mspeed * Mathf.Cos((get_sea.y + 90) / 180 * Mathf.PI);
+                //global_x[2] = -mspeed * Mathf.Sin((get_sea.y + 90) / 180 * Mathf.PI);
+            }
         }
-        //後に進む
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddForce(-transform.forward * mspeed);
-        }
-        //右に進む
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(transform.right * mspeed);
-        }
-        //左に進む
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(-transform.right * mspeed);
-        }
-        
+
     }
 
     private void Update() 
     {
         Debug.Log(isON);
-        if (Input.GetKey(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F))
         {
             ChageLight();
         }
-        //keyを離したときに止まる
-        if (Input.GetKeyUp(KeyCode.W) ||
-            Input.GetKeyUp(KeyCode.A) ||
-            Input.GetKeyUp(KeyCode.S) ||
-            Input.GetKeyUp(KeyCode.D)) 
-        {
-            if (Input.GetKey(KeyCode.W)||
-                Input.GetKey(KeyCode.A)||
-                Input.GetKey(KeyCode.S)||
-                Input.GetKey(KeyCode.D))
+        //※keyを離したときに止まる
+        {/*
+            if (Input.GetKeyUp(KeyCode.W) ||
+                Input.GetKeyUp(KeyCode.A) ||
+                Input.GetKeyUp(KeyCode.S) ||
+                Input.GetKeyUp(KeyCode.D)) 
             {
+                if (Input.GetKey(KeyCode.W)||
+                    Input.GetKey(KeyCode.A)||
+                    Input.GetKey(KeyCode.S)||
+                    Input.GetKey(KeyCode.D))
+                {
                 
-            }
-            else
-            {
-                rb.velocity = Vector3.zero;
-            }
+                }
+                else
+                {
+                    rb.velocity = Vector3.zero;
+                }
+            }*/
         }
 
         //Shiftキーでダッシュする
-        if (Input.GetKey(KeyCode.LeftShift))
         {
-            SaveSpeed = 5;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                //SaveSpeed = 5;
+                mspeed = 4;
+            }
+            else
+            {
+                //SaveSpeed = 3;
+                mspeed = 2;
+            }
+
+            if (keyCount >= 2)
+            {
+                mspeed = mspeed / Mathf.Sqrt(2);
+                //Debug.Log(Mathf.Sqrt(2));
+
+            }
         }
-        else
+        //WASD keyのどれかが押されたときの反応(移動)
         {
-            SaveSpeed = 3;
+            if (key_W)
+            {
+                //rb.AddForce(transform.forward * mspeed);
+                global_z[1] = mspeed * Mathf.Cos(get_see.y / 180 * Mathf.PI);
+                global_x[1] = mspeed * Mathf.Sin(get_see.y / 180 * Mathf.PI);
+            }
+            //後に進む
+            if (key_S)
+            {
+                //rb.AddForce(-transform.forward * mspeed);
+                global_z[3] = -mspeed * Mathf.Cos(get_see.y / 180 * Mathf.PI);
+                global_x[3] = -mspeed * Mathf.Sin(get_see.y / 180 * Mathf.PI);
+            }
+            //右に進む
+            if (key_D)
+            {
+                //rb.AddForce(transform.right * mspeed);
+                global_z[4] = mspeed * Mathf.Cos((get_see.y + 90) / 180 * Mathf.PI);
+                global_x[4] = mspeed * Mathf.Sin((get_see.y + 90) / 180 * Mathf.PI);
+            }
+            //左に進む
+            if (key_A)
+            {
+                //rb.AddForce(-transform.right * mspeed);
+                global_z[2] = -mspeed * Mathf.Cos((get_see.y + 90) / 180 * Mathf.PI);
+                global_x[2] = -mspeed * Mathf.Sin((get_see.y + 90) / 180 * Mathf.PI);
+            }
+
+            global_x[0] = global_x[1] + global_x[2] + global_x[3] + global_x[4];
+            global_z[0] = global_z[1] + global_z[2] + global_z[3] + global_z[4];
+
+            rb.velocity = new Vector3(global_x[0], global_y[0], global_z[0]);
+
+            Debug.Log(rb.velocity);
         }
     }
     private void ChageLight()
