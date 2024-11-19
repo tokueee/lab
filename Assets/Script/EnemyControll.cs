52cfc8d33257fce3be4015bc6dd1ec172fc6baeb
hiroto-onozawa <s2223026@cco.kanagawa-it.ac.jp>
2024-11-19 23:30:01 +0900

    エネミーとの距離に応じてノイズの強さ変動させるようにした

--- Assets/Script/EnemyControll.cs
+++ Assets/Script/EnemyControll.cs
@@ -23,33 +23,81 @@ public class EnemyControll : MonoBehaviour
 
     private int destNum = 0;
 
-    public GameObject Cnoi;
-    CameraNoise noise;
+    public GameObject Cnoi;//Script取得用
+    [SerializeField] private GameObject enemy;//距離取得の為にエネミー取得
+    CameraNoise noise;//Script取得用2
+    private float dis;//距離計算後の代入用変数
+
+    //ノイズの透過度の値
+    private float lmax = 0.1f;
+    private float lightmin_c = 0.08f;
+    private float lmid = 0.06f;
+    private float lmin = 0.04f;
+
+    //距離用の変数
+    private float cdist = 10.0f;
+    private float mdist = 20.0f;
+    private float fcdist = 30.0f;
+    private float fdists = 40.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         nevMeshAgent = GetComponent<NavMeshAgent>();
         nevMeshAgent.destination = Goal[destNum].position;
-
         noise = Cnoi.GetComponent<CameraNoise>();
+        noise.setTrans(0.0f);//透過の初期化
+        //Debug.Log(noise.getTrans());
+    }
+
+    //距離に応じて返す関数
+    private int DisNum(float dist)
+    {
+        if (dist < cdist) return 0;
+        if (dist < mdist) return 1;
+        if (dist < fcdist) return 2;
+        if (dist < fdists) return 3;
+        return 4;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         CheckPlayer();
+        dis = Vector3.Distance(enemy.transform.position,player.transform.position);
+        //プレイヤーと敵の距離計算
+        //Debug.Log(dis);
+        int disnum = DisNum(dis);
+        //Debug.Log(disnum);
+        switch (disnum)
+        {
+            //disnumの値に応じて実行する
+            case 0:
+                noise.setTrans(lmax) ;
+                noise.enabled = true; break;
+            case 1:
+                noise.setTrans(lightmin_c) ;
+                noise.enabled = true; break;
+            case 2: 
+                noise.setTrans(lmid) ;
+                noise.enabled = true; break;
+            case 3:
+                noise.setTrans(lmin) ;
+                noise.enabled = true; break;
+            case 4:
+                noise.setTrans(0.0f);
+                noise.enabled = true; break;
+        }
         // Debug.Log(Enemy_Angle);
         // Debug.Log(Enemy_View);
         if (isChasing == true)
         {
             nevMeshAgent.destination = player.transform.position;
-            noise.enabled = true;
         }
         else
         {
             if (nevMeshAgent.remainingDistance < 0.5f) { Patrol(); }
-            noise.enabled=false;
         }
     }
 
@@ -70,8 +118,8 @@ public class EnemyControll : MonoBehaviour
                 if (Physics.Raycast(transform.position, DirectionToPlayer, out hit, Enemy_View))
                 {
                     // Rayが当たった位置をデバッグログに表示
-                    Debug.Log("Ray hit position: " + hit.point);
-                    Debug.Log("Hit distance: " + hit.distance); // ヒットした距離を表示
+                    //Debug.Log("Ray hit position: " + hit.point);
+                    //Debug.Log("Hit distance: " + hit.distance); // ヒットした距離を表示
                     if (hit.collider.gameObject == player)
                     {
                         isChasing = true; return;
